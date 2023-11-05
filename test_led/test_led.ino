c6fd9e390444301b83d9159b476ef1ab0787aebd
mikehetu <michael.hetu@mail.mcgill.ca>
2023-11-05 00:50:17 -0400

    end of day saturday

--- test_led/test_led.ino
+++ test_led/test_led.ino
@@ -3,18 +3,67 @@
 #define LED_PIN 6
 #define LED_COUNT 16
 
+const byte numChars = 3;
+char receivedChars[numChars];  
+char tempChars[numChars];       
+
+int R = 0;
+int G = 0;
+int B = 0;
+int W = 0;
+
+boolean newData = false;
+
 Adafruit_NeoPixel ring(LED_COUNT, LED_PIN, NEO_RGBW + NEO_KHZ800);
+
+void recvWithEndMarker() {
+    static byte ndx = 0;
+    char endMarker = '\n';
+    char rc;
+    
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (rc != endMarker) {
+            receivedChars[ndx] = rc;
+            ndx++;
+            if (ndx >= numChars) {
+                ndx = numChars - 1;
+            }
+        }
+        else {
+            receivedChars[ndx] = '\0'; // terminate the string
+            ndx = 0;
+            newData = true;
+        }
+    }
+}
+
+void showNewData() {
+    if (newData == true) {
+        Serial.print("This just in ... ");
+        Serial.println(receivedChars);
+        newData = false;
+    }
+}
+
 
 void setup() {
   ring.begin();           
   ring.show();  
   ring.setBrightness(50);
+  Serial.begin(9600);
 }
 
 void loop() {
-  for (int i=0; i<=16; i++) {
-    ring.setPixelColor(i, 255, 255, 255, 0);  
+    recvWithEndMarker();
+    showNewData();
+
+    R = int(receivedChars[0]) + int(receivedChars[1]) ;
+    Serial.println(R);
+
+
+    ring.setPixelColor(1, R, G, B, W);
+    ring.show();
   }
-  ring.show(); 
-     
-}
+    
